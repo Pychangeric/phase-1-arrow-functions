10a113f31e446a80065b90714e48793be87fdcf6
pychangeric <pychangeric@gmail.com>
2023-03-15 10:41:42 +0300

    compeleted assingment

---
+++ index.js
@@ -0,0 +1,21 @@
+const divide = function() {
+    return '20';
+    
+}
+ const division = (parameter1, parameter2) => parameter1 / parameter2;
+  divide(200,100);
+
+  const square = function() {
+    return 'square';
+}
+const nums = [2,2];
+const squares = nums.map(x => x ** 2); 
+squares;// => [4]
+nums;
+
+let sum=function() {
+    return 'add';
+}
+let a=3
+let b=4
+let add = (parameter1, parameter2) => parameter1 + parameter2;
